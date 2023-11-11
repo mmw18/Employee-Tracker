0a56f4b8b9ea3b0e62e457226718ba01fe1dafd3
Megan Wright <meganmwright2018@gmail.com>
2023-11-11 01:37:59 -0600

    Cleaning up and commenting .js files

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,31 +1,17 @@
-USE centralPerk_db; 
+USE centralPerk_db;
 
+-- Departments
 INSERT INTO department (name) VALUES ('Wait-Staff'), ('Kitchen'), ('Entertainment'), ('Human-Resources'), ('Finance'), ('Operations');
 
+-- Roles and their: salary and department ID
 INSERT INTO role (title, salary, department_id) VALUES ('Chef', 55000, 2), ('Waiter/Waitress', 20000, 1), ('HR Specialist', 55000, 4), ('Operations Manager', 74000, 6), ('Singer', 20000, 3), ('Actor', 20000, 3), ('Accountant', 73500, 5);
 
+-- Employees (first and Last name), their employee ID, and their manager's employee ID
 INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES 
-('Chandler', 'Bing', 7, 1),
-('Phoebe', 'Buffay', 5, 1),
-('Monica', 'Geller', 1, 1),
-('Ross', 'Geller', 3, 1),
+('Chandler', 'Bing', 7, 4),
+('Phoebe', 'Buffay', 5, 4),
+('Monica', 'Geller', 1, 4),
+('Ross', 'Geller', 3, 4),
 ('Rachel', 'Green', 4, NULL),
-('Janice', 'Hosenstein', 2, 1),
-('Joey', 'Tribbiani', 6, 1);
-
-
-
--- USE centralPerk_db; 
-
--- INSERT INTO department (name) VALUES ('Wait-Staff'), ('Kitchen'), ('Entertainment'), ('Human-Resources'), ('Finance'), ('Operations');
-
--- INSERT INTO role (title, salary, department_id) VALUES ('Chef', 55000, 2), ('Waiter/Waitress', 20000, 1), ('HR Specialist', 55000, 4), ('Operations Manager', 74000, 6), ('Singer', 20000, 3), ('Actor', 20000, 3), ('Accountant', 73500, 5),;
-
--- INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES 
--- ('Chandler', 'Bing', 7, 1),
--- ('Phoebe', 'Buffay', 5, 1),
--- ('Monica', 'Geller', 1, 1),
--- ('Ross', 'Geller', 3, 1),
--- ('Rachel', 'Green', 4, NULL),
--- ('Janice', 'Hosenstein', 2, 1),
--- ('Joey', 'Tribbiani', 6, 1);
+('Janice', 'Hosenstein', 2, 4),
+('Joey', 'Tribbiani', 6, 4);
