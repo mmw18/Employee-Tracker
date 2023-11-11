e061d484009242edca211d4ed743d38c30375d8c
Megan Wright <meganmwright2018@gmail.com>
2023-11-10 21:28:51 -0600

    TroubleShooting schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -24,5 +24,37 @@ CREATE TABLE employee (
   role_id INT,
   manager_id INT NULL,
   FOREIGN KEY (role_id) REFERENCES role(id),
-  FOREIGN KEY (manager_id) REFERENCES employee(id)
+  FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE CASCADE
 );
+
+
+
+
+-- CREATE DATABASE IF NOT EXISTS centralPerk_db;
+-- USE centralPerk_db; 
+
+-- -- Table for DEPARTMENT information
+-- CREATE TABLE department (
+--   id INT AUTO_INCREMENT PRIMARY KEY,
+--   name VARCHAR(30) NOT NULL
+-- );
+
+-- -- Table for ROLE information
+-- CREATE TABLE role (
+--   id INT AUTO_INCREMENT PRIMARY KEY,
+--   title VARCHAR(30) NOT NULL,
+--   salary DECIMAL(10, 2) NOT NULL,
+--   department_id INT,
+--   FOREIGN KEY (department_id) REFERENCES department(id)
+-- );
+
+-- -- Table for EMPLOYEE information
+-- CREATE TABLE employee (
+--   id INT AUTO_INCREMENT PRIMARY KEY,
+--   first_name VARCHAR(30) NOT NULL,
+--   last_name VARCHAR(30) NOT NULL,
+--   role_id INT,
+--   manager_id INT NULL,
+--   FOREIGN KEY (role_id) REFERENCES role(id),
+--   FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE CASCADE
+-- );
