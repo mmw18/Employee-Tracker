58cd4dbe44d77b8a3941b1ed5550b13d571ea73c
Megan Wright <meganmwright2018@gmail.com>
2023-11-11 11:19:48 -0600

    Correcting index.js, fixed error in seeds.sql

--- index.js
+++ index.js
@@ -41,123 +41,48 @@ async function init() {
     switch (answer.action) {
       case 'View all employees':
         await employee.viewAll();
+        init(); // Restarting prompt after operation
         break;
       case 'Add employee':
         await employee.add();
+        init(); // Restarting prompt after operation
         break;
       case 'Update employee role':
         await employee.updateRole();
+        init(); // Restarting prompt after operation
         break;
       case 'View all roles':
         await role.viewAll();
+        init(); // Restarting prompt after operation
         break;
       case 'Add role':
         await role.add();
+        init(); // Restarting prompt after operation
         break;
       case 'View all departments':
         await department.viewAll();
+        init(); // Restarting prompt after operation
         break;
       case 'Add department':
         await department.add();
+        init(); // Restarting prompt after operation
         break;
-      // case 'Exit': exiting application
+      case 'Exit':
+        // case 'Exit': exiting application
         console.log('Goodbye!');
         connection.end();
-        return;
+        return; // Exiting, do not restart prompt
       default:
         // handles unexpected or invalid inputs by logging an error message and then breaking out of the switch statement
         console.log(`Invalid action: ${answer.action}`);
+        init(); // Restarting prompt in case of invalid input
         break;
     }
-    // restarting prompt
-    init();
   } catch (error) {
     // Logging an error if one is caught
     console.error('Error:', error);
-    init();
+    init(); // Restarting prompt in case of error
   }
 }
 // Prompting user when application is started
 init();
-
-
-
-
-// const inquirer = require('inquirer');
-// const mysql = require('mysql2');
-// require('dotenv').config();
-
-// // Importing modules being used
-// const Department = require('./lib/department');
-// const Role = require('./lib/role');
-// const Employee = require('./lib/employee');
-// const query = require('./lib/query');
-
-// const connection = mysql.createConnection({
-//   host: 'localhost',
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   database: process.env.DB_NAME
-// });
-
-// const department = new Department(connection);
-// const role = new Role(connection);
-// const employee = new Employee(connection);
-
-// function init() {
-//   inquirer.prompt({
-//     type: 'list',
-//     name: 'action',
-//     message: 'What would you like to do?',
-//     choices: [
-//       'View all employees',
-//       'Add employee',
-//       'Update employee role',
-//       'View all roles',
-//       'Add role',
-//       'View all departments',
-//       'Add department',
-//       'Exit'
-//     ]
-//   })
-//   .then((answer) => {
-//     switch (answer.action) {
-//       case 'View all employees':
-//         employee.viewAll(); 
-//         break;
-//       case 'Add employee':
-//         employee.add(); 
-//         break;
-//       case 'Update employee role':
-//         employee.updateRole(); 
-//         break;
-//       case 'View all roles':
-//         role.viewAll(); 
-//         break;
-//       case 'Add role':
-//         role.add(); 
-//         break;
-//       case 'View all departments':
-//         department.viewAll();
-//         break;
-//       case 'Add department':
-//         department.add();
-//         break;
-//       case 'Exit':
-//         console.log('Goodbye!');
-//         connection.end();
-//         break;
-//       default:
-//         console.log(`Invalid action: ${answer.action}`);
-//         init(); // Restarting prompt
-//         break;
-//     }
-//   })
-//   .catch((error) => {
-//     console.error('Error:', error);
-//     init(); // Restarting prompt in case of error
-//   });
-// }
-
-// // Starting the application
-// init();
